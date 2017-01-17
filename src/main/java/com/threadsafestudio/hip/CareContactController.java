5b904c6b2569a0d87a758aad4fc71a21cec00b88
Pascal Chatterjee <pascal.chatterjee@gmail.com>
2017-01-17 14:14:22 +0100

    Use care-contact service instead, exactly like "Getting HIP:ed" instructions

--- src/main/java/com/threadsafestudio/hip/CareContactController.java
+++ src/main/java/com/threadsafestudio/hip/CareContactController.java
@@ -6,11 +6,11 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import se.hip.sdk.api.Api;
-import se.hip.sdk.api.GetCareDocumentationBuilder;
+import se.hip.sdk.api.GetCareContactsBuilder;
 import se.hip.sdk.api.RegisterPdlConsentBuilder;
-import se.hip.sdk.api.caredocumentation.CareDocumentation;
+import se.hip.sdk.api.carecontacts.CareContact;
 import se.hip.sdk.api.core.*;
-import se.hip.sdk.api.operation.GetCareDocumentation;
+import se.hip.sdk.api.operation.GetCareContacts;
 import se.hip.sdk.api.operation.RegisterPdlConsent;
 import se.hip.sdk.api.query.result.DataResultSet;
 
@@ -18,13 +18,13 @@ import se.hip.sdk.api.query.result.DataResultSet;
  * Created by pascal on 2017-01-10.
  */
 @Controller
-public class CareDocumentationController {
+public class CareContactController {
     @Autowired
     private Api api;
 
-    @RequestMapping("/caredocumentation")
+    @RequestMapping("/carecontacts")
     @ResponseBody
-    public Response<DataResultSet<CareDocumentation>> careDocumentation() {
+    public Response<DataResultSet<CareContact>> careDocumentation() {
         final CareActorPrincipal careActor = PrincipalFactory.createCareActorPrincipal(
                 "sessionId",
                 "Vård och behandling",
@@ -51,11 +51,11 @@ public class CareDocumentationController {
 
         final Response resp = postConsent.execute();
         if (resp.getStatus().getErrors().isEmpty()) {
-            final GetCareDocumentation request = api.getOperationBuilder(GetCareDocumentationBuilder.class)
+            final GetCareContacts request = api.getOperationBuilder(GetCareContactsBuilder.class)
                     .as(careActor)
                     .withSubjectOfCare(DefaultSubjectOfCare.create("191212121212"))
                     .build();
-            final Response<DataResultSet<CareDocumentation>> response = request.execute();
+            final Response<DataResultSet<CareContact>> response = request.execute();
             return response;
         } else {
             return null;
